895eb2450a15a78988bc02a366cc045cdc6b1264
Ang Shu Khai <80701138+ASK0118@users.noreply.github.com>
2021-06-05 18:20:08 +0800

    Delete Assignment2 directory

--- Assignment2/Payment.java
+++
@@ -1,9 +0,0 @@
-package Assignment2;
-
-public interface Payment {  //2.5 Interface
-	
-	double discount();
-	double Rentprice(int selection,int choose, int day);
-	double getPayment(int selection,int choose,int day);
-	double getPayment(int selection,int choose, int day, double discount);
-}
